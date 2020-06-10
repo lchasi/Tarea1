2567f1b62145c339d9cadd95f1179e03378867d2
Lenin Chasi <64288777+lchasi@users.noreply.github.com>
2020-06-09 20:09:04 -0500

    summary primary

---
+++ src/tarea1/TareaUno.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea1;
+
+/**
+ *
+ * @author lchas
+ */
+public class TareaUno {
+    
+    private String nombre;
+  private String apellido;
+  private int edad;
+  private boolean sexo;
+  private String estadocivil;
+  private String usuario;
+  private String password;
+
+    public TareaUno(String nombre, String apellido, int edad, boolean sexo, String estadocivil, String usuario, String password) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.sexo = sexo;
+        this.estadocivil = estadocivil;
+        this.usuario = usuario;
+        this.password = password;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public boolean isSexo() {
+        return sexo;
+    }
+
+    public void setSexo(boolean sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getEstadocivil() {
+        return estadocivil;
+    }
+
+    public void setEstadocivil(String estadocivil) {
+        this.estadocivil = estadocivil;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+    
+    
+    
+}
